8433fd0510f4e3f75a5c91fa7cbee44679ef350d
elijah-g-14 <69686507+elijah-g-14@users.noreply.github.com>
2021-09-20 13:05:21 -0400

    starting to use Joi

---
+++ index.js
@@ -0,0 +1,43 @@
+const Joi = require('joi');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const courses = [
+    { id:1, name: 'course1' },
+    { id:2, name: 'course2' },
+    { id:3, name: 'course3' },
+];
+app.get('/', (req,res) =>{
+    res.send('Hello World');
+});
+
+app.get('/api/courses', (req,res) => {
+    res.send(courses);
+});
+
+app.post('/api/courses', (req, res) => {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    const result = Joi.valid(req.body, schema);
+    console.log(result);
+    
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+})
+
+app.get('/api/courses/:id', (req,res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('The course with the given ID was not found');
+    res.send(course);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
