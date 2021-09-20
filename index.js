f93ca6f35b9ca0df06031d6a2cdd0e45c6d2dd51
elijah-g-14 <69686507+elijah-g-14@users.noreply.github.com>
2021-09-20 19:10:29 -0400

    completed the put method

--- index.js
+++ index.js
@@ -17,27 +17,55 @@ app.get('/api/courses', (req,res) => {
     res.send(courses);
 });
 
-app.post('/api/courses', (req, res) => {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
 
-    const result = Joi.valid(req.body, schema);
-    console.log(result);
-    
+app.post('/api/courses', (req, res) => {
+    const { error } = validateCourse(req.body); //Object deconst.
+    if (error){
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
     const course = {
         id: courses.length + 1,
         name: req.body.name
     };
     courses.push(course);
     res.send(course);
-})
+});
+
+app.put('/api/courses/:id', (req,res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('The course with the given ID was not found');
+    
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    
+    const { error } = validateCourse(req.body); //Object deconst.
+    if (error){
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    course.name = req.body.name;
+    res.send(course);
+
+});
+
 
 app.get('/api/courses/:id', (req,res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
     if(!course) res.status(404).send('The course with the given ID was not found');
     res.send(course);
 });
+
+function validateCourse(course){
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    
+    return Joi.validate(course, schema);
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
