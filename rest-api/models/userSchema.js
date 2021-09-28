f1d6887a70be885bb78fc0f985416ff00ec47b52
Blistex <blistex@Blistexs-MacBook-Pro.local>
2021-09-27 22:30:24 -0400

    server started again

---
+++ rest-api/models/userSchema.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const user = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", user);
+
+module.exports = router;
