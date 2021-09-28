3658f1b437caf66e8bd41a6271620bb588dc431a
elijah-g-14 <69686507+elijah-g-14@users.noreply.github.com>
2021-09-28 11:56:52 -0400

    Used bulit in and created validation.

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -5,11 +5,40 @@ mongoose.connect('mongodb://localhost/playground')
  .catch(err => console.error('Could not connect to MongoDB...', err))
 
 const courseSchema = new mongoose.Schema({
-    name: String, 
+    name: {
+            type: String, 
+            required: true,
+            minlength: 5,
+            maxlength: 255
+
+    },   //Bulitin validation
+    category: {
+        type: String,
+        required: true,
+        enum: ['web', 'mobile', 'network'],
+        lowercase: true,
+        trim: true
+    },
     author: String,
-    tags: [String],
+    tags: {
+        type: Array,
+        validate: {
+            validator: function(v, callback) {  //To validate that there are no empty arrays
+                return v && v.length > 0; 
+            },
+            message: 'A course should have at least one tag'
+        }
+    },
     date: { type: Date, default: Date.now},
-    isPublished: Boolean
+    isPublished: Boolean,
+    price: {
+        type: Number,
+        required: function() {return this.isPublished;},
+        min: 10,
+        max: 200,
+        get: v => Math.round(v),
+        set: v => Math.round(v)
+    }
 });
 
 const Course = mongoose.model('Course', courseSchema);
@@ -17,13 +46,22 @@ const Course = mongoose.model('Course', courseSchema);
 async function createCourse(){
     const course = new Course({
         name: 'Angular Course',
+        category: 'Web',
         author: 'Elijah',
-        tags: ['angular', 'frontend'],
-        isPublished: true
+        tags: ['frontend'],
+        isPublished: true,
+        price: 15.8
     });
 
-    const result = await course.save();
-    console.log(result);
+    try{
+        // course.validate();
+        const result = await course.save();
+        console.log(result);
+    }catch (ex) {
+        for (field in ex.errors)
+            console.log(ex.errors[field].message);
+    }
+    
 };
 
 async function getCourses(){
@@ -31,15 +69,35 @@ async function getCourses(){
     const pageSize = 10;
 
     const courses = await Course
-        .find({ author:'Elijah', isPublished: true})
+        .find({ _id: '615339b7dc73be793bc990c6'})
         //.find({author: /^Elijah/})    //starts with
         // .find({author: /Goodman$/i}) // ends with
         // .find({author: /*Elijah*/i}) //contains string
         //.or([{ author: 'Elijah'}, {isPublished: true }])
         // .skip((pageNumber -1) * pageSize)
         // .limit(pageSize)
-        // .sort({ name: 1})
-    console.log(courses);
+        .sort({ name: 1})
+        .select({ name: 1, tags: 1, price: 1});
+    console.log(courses[0].price);
+}
+
+async function updateCourse(id){
+    const course = await Course.findByIdAndUpdate(id, {
+        $set: {
+            author: 'Jason',
+            isPublished: false
+        }
+    }, {new: true});
+    
+    console.log(course);
+}
+
+async function removeCourse(id){
+    const result = await Course.deleteMany({ _id: id});
+    const course = await Course.findByIdAndRemove(id);
+    console.log(course);
 }
 
 getCourses();
+// removeCourse('6152292e4b35cd9a2a7b80ce');
+
