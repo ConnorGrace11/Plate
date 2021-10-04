3cb78fbd57793dad8740a08bb6792779ad028a89
ConnorGrace11 <70079448+ConnorGrace11@users.noreply.github.com>
2021-10-03 23:18:57 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,183 @@
+import * as React from 'react';
+import LoginScreen from './src/screens/LoginScreen';
+import HomeScreen from './src/screens/HomeScreen';
+import CreateAccountScreen from './src/screens/CreateAccountScreen';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+const AppNavigator = createStackNavigator(
+
+    {
+      Home: HomeScreen,
+      Login: LoginScreen,
+      CreateAccount: CreateAccountScreen,
+    },
+    {
+      initialRouteName: 'Home',
+    },
+  );
+
+  export default createAppContainer(AppNavigator);
+// import { StatusBar } from "expo-status-bar";
+// import React, { useState } from "react";
+// import { useEffect } from "react";
+// import {
+//   StyleSheet,
+//   Text,
+//   View,
+//   Image,
+//   TextInput,
+//   Button,
+//   TouchableOpacity,
+//   Alert,
+//   ActivityIndicator,
+//   FlatList,
+// } from "react-native";
+//
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+//
+// const Stack = createNativeStackNavigator();
+//
+// function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen name="Login" component={LoginScreen} />
+//         <Stack.Screen name="Movies" component={MoviesScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+//
+// function LoginScreen({ navigation }) {
+//
+//     const [email, setEmail] = useState("");
+//     const [password, setPassword] = useState("");
+//
+//     return (
+//
+//       <View style={styles.container}>
+//         <Image style={styles.image} source={require("./assets/plate.png")} />
+//
+//         <StatusBar style="auto" />
+//         <View style={styles.inputView}>
+//           <TextInput
+//             style={styles.TextInput}
+//             placeholder="Email."
+//             placeholderTextColor="#003f5c"
+//             onChangeText={(email) => setEmail(email)}
+//           />
+//         </View>
+//
+//         <View style={styles.inputView}>
+//           <TextInput
+//             style={styles.TextInput}
+//             placeholder="Password."
+//             placeholderTextColor="#003f5c"
+//             secureTextEntry={true}
+//             onChangeText={(password) => setPassword(password)}
+//           />
+//         </View>
+//
+//         <TouchableOpacity style={styles.forgot_button} onPress={() => Alert.alert('That sucks.')}>
+//           <Text style={styles.forgot_button}>Forgot Password?</Text>
+//         </TouchableOpacity>
+//
+//         <TouchableOpacity style={styles.loginBtn} onPress={() => navigation.navigate('Movies')}>
+//           <Text style={styles.loginText}>LOGIN</Text>
+//         </TouchableOpacity>
+//       </View>
+//
+//
+//   );
+// }
+//
+// function MoviesScreen() {
+//   const [isLoading, setLoading] = useState(true);
+//   const [data, setData] = useState([]);
+//
+//   const getMovies = async () => {
+//     try {
+//       const response = await fetch('https://reactnative.dev/movies.json');
+//       const json = await response.json();
+//       setData(json.movies);
+//     } catch (error) {
+//       console.error(error);
+//     } finally {
+//       setLoading(false);
+//     }
+//   }
+//   useEffect(() => {
+//     getMovies();
+//   }, []);
+//
+//   return (
+//     <View style={{ flex: 1, padding: 24 }}>
+//       {isLoading ? <ActivityIndicator/> : (
+//         <FlatList
+//           data={data}
+//           keyExtractor={({ id }, index) => id}
+//           renderItem={({ item }) => (
+//           <Text>{item.title}, {item.releaseYear}</Text>
+//           )}
+//         />
+//       )}
+//     </View>
+//   );
+// }
+//
+//
+// <Stack.Screen
+//   name="Home"
+//   component={HomeScreen}
+//   options={{ title: 'Overview' }}
+// />
+//
+// export default App;
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//
+//   image: {
+//     marginBottom: 40,
+//   },
+//
+//   inputView: {
+//     backgroundColor: "orange",
+//     borderRadius: 30,
+//     width: "70%",
+//     height: 45,
+//     marginBottom: 20,
+//
+//     alignItems: "center",
+//   },
+//
+//   TextInput: {
+//     height: 50,
+//     flex: 1,
+//     padding: 10,
+//     marginLeft: 20,
+//     color: 'black'
+//   },
+//
+//   forgot_button: {
+//     height: 30,
+//     marginBottom: 30,
+//   },
+//
+//   loginBtn: {
+//     width: "80%",
+//     borderRadius: 25,
+//     height: 50,
+//     alignItems: "center",
+//     justifyContent: "center",
+//     marginTop: 40,
+//     backgroundColor: "#ff8c00",
+//   },
+// });
