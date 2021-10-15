1cc3ff9900ff0dd714ae52da6b5af9ca8d8e14ef
elijah-g-14 <69686507+elijah-g-14@users.noreply.github.com>
2021-10-15 13:01:11 -0400

    Fixed Restaurants Patch and naming conventions

--- this-server/server.js
+++ this-server/server.js
@@ -5,6 +5,8 @@ const app = express();
 const mongoose = require('mongoose');
 const auth = require('./routes/auth');
 const meals = require('./routes/meal');
+const restaurants = require('./routes/restaurant');
+const cors = require('cors');
 // const fs = require('fs');
 // const path = require('path');
 // const multer = require('multer');
@@ -20,8 +22,10 @@ db.on('error', (error) => console.log(error))
 db.once('open', () => console.log('Connected to database!'))
 
 app.use(express.json());
+app.use(cors());
 app.use('/api/auth', auth);
 app.use('/meals', meals);
+app.use('/restaurants', restaurants);
 
 const PORT = process.env.PORT || 3031;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
