d97f739c244de55b0c74709e2a9dc7cbf9811fd2
Dorizabelle <dorizabellen@gmail.com>
2021-11-10 18:26:53 -0500

    reviewtr1

--- RNB Up/Plate/my-plate/src/components/pages/reviews.js
+++ RNB Up/Plate/my-plate/src/components/pages/reviews.js
@@ -1,68 +1,47 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./styles.css";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './AllMeals.css';
 
-class FiveStars extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      starsArr: [0,0,0,0,0],
-      oldArr: [0,0,0,0,0]
-    };
+const AllRestaurants = () => {
+    
+    useEffect(() => {
+        getRestaurants()
+    }, [])
 
-    this.handleStarsHover = this.handleStarsHover.bind(this);
-    this.handleStarsClick = this.handleStarsClick.bind(this);
-    this.handleStarsLeave = this.handleStarsLeave.bind(this);
-  }
+    const [restaurants, setRestaurants] = useState("");
+    const [loading, setLoading] = useState(false);
 
-  handleStarsHover(event) {
-    event.preventDefault();
-    let rating = parseInt(event.target.getAttribute("value"))+1;
-    let newArr = [];
-    while (newArr.length < 5) {
-        if (rating > 0) {
-            rating--;
-            newArr.push(1);
-        } else {
-            newArr.push(0);
+    const getRestaurants = async () => {
+        try {
+            const restaurantResponse = await axios.get("http://localhost:3000/restaurants"/* ,{
+                headers: {
+                    'Access-Control-Allow-Origin': '*'
+                }
+            } */)
+            setRestaurants(restaurantResponse.data)
+            setLoading(true) 
+        } catch (error) {
+            console.log(error.message)
         }
     }
-    this.setState({
-        starsArr: newArr
-    });
-  }
 
-  handleStarsClick(event) {
-      event.preventDefault();
-      this.setState({
-          oldArr: this.state.starsArr
-      });
-  }
-
-  handleStarsLeave(event) {
-      event.preventDefault();
-      this.setState({
-          starsArr: this.state.oldArr
-      });
-  }
-
-  render() {
     return (
-    <div>
-      <h1>Rate out of 5 Stars</h1>
-      {this.state.starsArr.map((item, i) => {
-          return (
-              <div className="single-star-container" value={i} key={i} onMouseOver={this.handleStarsHover} onClick={this.handleStarsClick} onMouseLeave={this.handleStarsLeave}>
-                  <div className="single-star-fill" style={{"width" : `${parseInt(item*31)}px`}}>
-                      <img className="single-star-outline" src="star.png" value={i} ></img>
-                  </div>
-              </div>
-          );
-        })}
-    </div>
+        <>
+        <div className="all-items">
+            <h1>RESTAURANTS</h1>
+        </div>
+        <div>
+        {loading && 
+            restaurants.map( (item) => (
+                <div>
+                    <h2>{ item.name }</h2>
+                    <h5>{ item.location }</h5>
+                    <p>{ item.rating }</p>   
+                </div>
+            ))}      
+        </div>
+        </>
     );
-  }
 }
 
-var mountNode = document.getElementById("app");
-ReactDOM.render(<FiveStars/>, mountNode);
+export default AllRestaurants;
