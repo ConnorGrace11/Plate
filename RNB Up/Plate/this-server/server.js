ba445cc186bef915a45331d7424eb8adebc13a33
Dorizabelle <dorizabellen@gmail.com>
2021-11-10 12:42:43 -0500

    reviewstr

--- RNB Up/Plate/this-server/server.js
+++ RNB Up/Plate/this-server/server.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose');
 const auth = require('./routes/auth');
 const meals = require('./routes/meal');
 const restaurants = require('./routes/restaurant');
+const reviews = require('./routes/reviews');
+
 const cors = require('cors');
 // const fs = require('fs');
 // const path = require('path');
@@ -26,7 +28,8 @@ app.use(cors());
 app.use('/api/auth', auth);
 app.use('/meals', meals);
 app.use('/restaurants', restaurants);
+app.use('/reviews', reviews);
+
 
 const PORT = process.env.PORT || 3031;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
