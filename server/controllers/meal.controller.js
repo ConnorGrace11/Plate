efb8778f81394e0e9f8b62198276aad7478def64
ConnorGrace11 <70079448+ConnorGrace11@users.noreply.github.com>
2021-11-25 14:35:06 -0500

    server updates

--- server/controllers/meal.controller.js
+++ server/controllers/meal.controller.js
@@ -1,13 +1,9 @@
 const Meal = require('../models/meals')
-const _ = require('underscore')
-const fs = require('fs')
-const upload = require("../cloudHelper").upload;
-
 
 exports.getAllMeals = async (req, res) => {
     try {
         const grabMeals = await Meal.find();
-        res.status(201).json(grabMeals);
+        res.status(200).json(grabMeals);
     } catch (error) {
         res.status(400).json({ error: error.message })
     }
@@ -23,43 +19,21 @@ exports.getAMeal = async (req, res) => {
 };
 
 exports.createMeal = async (req, res) => {
-    const files = req.files
+    console.log(req.file)
+
+    const url = req.protocol + "://" + req.get('host')
+    const newMeal = new Meal({
+        name: req.body.name,
+        category: req.body.category,
+        todo: req.body.todo,
+        img: url + "/public/" + req.file.filename
+    })
+
     try {
-        let urls = [];
-        let multiple = async (path) => await upload(path);
-        for (const file of files){
-            const {path} = file;
-            console.log("path" , file);
-
-            const newPath = await multiple(path);
-            urls.push(newPath);
-            fs.unlinkSync(path);
-        }
-        if (urls) {
-            const newMeal = new Meal({
-                id: req.body.id,
-                name: req.body.name,
-                category: req.body.category,
-                description: req.body.description,
-                imgMeal: urls,
-                calories: req.body.calories,
-                allergens: req.body.allergens,
-                ingredients: req.body.ingredients
-            });
-            await newMeal.save()
-                .then(saved => {
-                    return res.json(saved)
-                }).catch(error => {
-                    return res.json(error);
-                })
-        }
-        if (!urls) {
-            return res.status(400)
-        }
-
+        const meal = await newMeal.save();
+        res.status(201).json(meal)
     } catch (error) {
-        console.log("error:  ", error);
-        return next(error);
+        return res.status(500).send({ message: error.message })
     }
 };
 
@@ -70,22 +44,8 @@ exports.editMeal = async (req, res) => {
     if(req.body.category != null) {
         res.editMeal.category = req.body.category
     }
-    if(req.body.description != null) {
-        res.editMeal.description = req.body.description
-    }
-    if(req.body.imgMeal != null){
-        res.editMeal.imgMeal = req.body.imgMeal
-    }
-    if(req.body.calories != null) {
-        res.editMeal.calories = req.body.calories
-    }
-    if(req.body.allergens != null){
-        // res.editMeal.allergens.push(req.editMeal.allergens)
-        // consol.log('This passed')
-        res.editMeal.allergens = req.editMeal.allergens
-    }
-    if(req.body.ingredients != null) {
-        res.editMeal.ingredients = req.body.ingredients
+    if(req.body.todo != null) {
+        res.editMeal.todo = req.body.todo
     }
     try{
         const modifiedMeal = await res.editMeal.save();
@@ -106,13 +66,121 @@ exports.deleteMeal = async (req, res) => {
     }
 }
 
-exports.getMealImg = async (req, res) => {
-    try {
-        const selectedMeal = await Meal.findById(req.params.id)
-        return res.status(200).json({ imgMeal: selectedMeal.imgMeal})
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-}
+// const Meal = require('../models/meals')
+// const _ = require('underscore')
+// const fs = require('fs')
+// const upload = require("../cloudHelper").upload;
 
-// https://www.youtube.com/watch?v=jn3tYh2QQ-g
+
+// exports.getAllMeals = async (req, res) => {
+//     try {
+//         const grabMeals = await Meal.find();
+//         res.status(201).json(grabMeals);
+//     } catch (error) {
+//         res.status(400).json({ error: error.message })
+//     }
+// };
+
+// exports.getAMeal = async (req, res) => {
+//     try {
+//         const oneMeal = await Meal.findById(req.params.id)
+//         return res.status(200).json({ meal: oneMeal })
+//     } catch (error) {
+//         res.status(400).json({ message: error.message });
+//     }
+// };
+
+// exports.createMeal = async (req, res) => {
+//     const files = req.files
+//     try {
+//         let urls = [];
+//         let multiple = async (path) => await upload(path);
+//         for (const file of files){
+//             const {path} = file;
+//             console.log("path" , file);
+
+//             const newPath = await multiple(path);
+//             urls.push(newPath);
+//             fs.unlinkSync(path);
+//         }
+//         if (urls) {
+//             const newMeal = new Meal({
+//                 id: req.body.id,
+//                 name: req.body.name,
+//                 category: req.body.category,
+//                 description: req.body.description,
+//                 imgMeal: urls,
+//                 calories: req.body.calories,
+//                 allergens: req.body.allergens,
+//                 ingredients: req.body.ingredients
+//             });
+//             await newMeal.save()
+//                 .then(saved => {
+//                     return res.json(saved)
+//                 }).catch(error => {
+//                     return res.json(error);
+//                 })
+//         }
+//         if (!urls) {
+//             return res.status(400)
+//         }
+
+//     } catch (error) {
+//         console.log("error:  ", error);
+//         return next(error);
+//     }
+// };
+
+// exports.editMeal = async (req, res) => {
+//     if(req.body.name != null) {
+//         res.editMeal.name = req.body.name
+//     }
+//     if(req.body.category != null) {
+//         res.editMeal.category = req.body.category
+//     }
+//     if(req.body.description != null) {
+//         res.editMeal.description = req.body.description
+//     }
+//     if(req.body.imgMeal != null){
+//         res.editMeal.imgMeal = req.body.imgMeal
+//     }
+//     if(req.body.calories != null) {
+//         res.editMeal.calories = req.body.calories
+//     }
+//     if(req.body.allergens != null){
+//         // res.editMeal.allergens.push(req.editMeal.allergens)
+//         // consol.log('This passed')
+//         res.editMeal.allergens = req.editMeal.allergens
+//     }
+//     if(req.body.ingredients != null) {
+//         res.editMeal.ingredients = req.body.ingredients
+//     }
+//     try{
+//         const modifiedMeal = await res.editMeal.save();
+//         res.json(modifiedMeal)
+//     } catch (error) {
+//         res.status(400).json({ message: error.message })
+//     }
+// };
+
+// exports.deleteMeal = async (req, res) => {
+
+//     try {
+//         await res.editMeal.remove();
+//         res.json({ message: "meal has been removed" })
+
+//     } catch (error) {
+//         res.status(500).json({ message: error.message })
+//     }
+// }
+
+// exports.getMealImg = async (req, res) => {
+//     try {
+//         const selectedMeal = await Meal.findById(req.params.id)
+//         return res.status(200).json({ imgMeal: selectedMeal.imgMeal})
+//     } catch (error) {
+//         res.status(400).json({ message: error.message });
+//     }
+// }
+
+// // https://www.youtube.com/watch?v=jn3tYh2QQ-g
