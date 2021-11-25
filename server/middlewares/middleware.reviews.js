f96ef729aeefc08beb196a801760768533d90cf0
ConnorGrace11 <70079448+ConnorGrace11@users.noreply.github.com>
2021-11-25 13:05:10 -0500

    reviews etc

---
+++ server/middlewares/middleware.reviews.js
@@ -0,0 +1,16 @@
+const Review = require('../models/reviews')
+
+exports.getReviewId = async (req, res, next) => {
+    let review;
+    try {
+        review = await Review.findById(req.params.reviewId)
+        if(review == null) {
+            return res.status(404).json({ message: 'Review not found.' })
+        }
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+
+    res.review = review
+    next()
+}
