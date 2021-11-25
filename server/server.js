bd980644d222ac5269096f9aff7bf78474132720
ConnorGrace11 <70079448+ConnorGrace11@users.noreply.github.com>
2021-11-25 14:41:17 -0500

    server updates

--- server/server.js
+++ server/server.js
@@ -1,19 +1,20 @@
 require('dotenv').config()
 
-const users = require('./models/users')
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const auth = require('./routes/auth');
 const meals = require('./routes/meal');
+const restaurants = require('./routes/restaurant');
 const cors = require('cors');
+const path = require('path');
+const multer = require('multer')
+const cloudinary = require('cloudinary').v2;
+const bodyParser = require('body-parser');
 
-// const fs = require('fs');
-// const path = require('path');
-// const multer = require('multer');
 
 mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true, 
+    useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
@@ -22,12 +23,45 @@ const db = mongoose.connection;
 db.on('error', (error) => console.log(error))
 db.once('open', () => console.log('Connected to database!'))
 
-app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(cors());
 app.use('/api/auth', auth);
 app.use('/meals', meals);
+app.use('/restaurants', restaurants);
 
 const PORT = process.env.PORT || 3031;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+// require('dotenv').config()
+
+// const users = require('./models/users')
+// const express = require('express');
+// const app = express();
+// const mongoose = require('mongoose');
+// const auth = require('./routes/auth');
+// const meals = require('./routes/meal');
+// const cors = require('cors');
+
+// // const fs = require('fs');
+// // const path = require('path');
+// // const multer = require('multer');
+
+// mongoose.connect(process.env.DATABASE_URL, {
+//     useNewUrlParser: true, 
+//     useUnifiedTopology: true
+// });
+
+// const db = mongoose.connection;
+
+// db.on('error', (error) => console.log(error))
+// db.once('open', () => console.log('Connected to database!'))
+
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }))
+// app.use('/api/auth', auth);
+// app.use('/meals', meals);
+
+// const PORT = process.env.PORT || 3031;
+// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
