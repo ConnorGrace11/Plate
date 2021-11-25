25238b215e3228fd896e87a2823145d4831e32ee
ConnorGrace11 <70079448+ConnorGrace11@users.noreply.github.com>
2021-11-25 14:25:22 -0500

    server updates

--- server/models/meals.js
+++ server/models/meals.js
@@ -9,26 +9,53 @@ const mealSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    description: {
+    todo: {
         type: String,
         required: true
     },
     imgMeal:
     {
         type: []
-    },
-    calories:
-    {
-        type: String
-    },
-    allergens:
-    {
-        type: []
-    },
-    ingredients:
-    {
-        type: String
     }
+    // imgMeal:
+    // {
+    //     type: String,
+    // }
 })
 
 module.exports = mongoose.model('Meals', mealSchema)
+
+// const mongoose = require('mongoose');
+
+// const mealSchema = new mongoose.Schema({
+//     name: {
+//         type: String,
+//         required: true
+//     },
+//     category: {
+//         type: String,
+//         required: true
+//     },
+//     description: {
+//         type: String,
+//         required: true
+//     },
+//     imgMeal:
+//     {
+//         type: []
+//     },
+//     calories:
+//     {
+//         type: String
+//     },
+//     allergens:
+//     {
+//         type: []
+//     },
+//     ingredients:
+//     {
+//         type: String
+//     }
+// })
+
+// module.exports = mongoose.model('Meals', mealSchema)
