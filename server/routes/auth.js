c76e6a299219f5423a63f46907d36a03675c2f9a
ConnorGrace11 <70079448+ConnorGrace11@users.noreply.github.com>
2021-11-25 13:09:14 -0500

    server files update

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,16 +1,35 @@
 const express = require('express');
 const router = express.Router();
-const control = require('../controllers/user.controller');
-const control2 = require('../controllers/auth.controller')
+const control = require('../controllers/auth.controller');
 const middleware = require('../middlewares/middleware.auth');
 
 // auth routes
 router.post('/login', control.logIn);
 router.post('/signup', control.signUp);
+// router.get('/logout/:username', middleware.getUserId, control.authenticateToken, control.logOut);
 
-router.get('/user/:username', middleware.getAuthId, control.authenticateToken, control.getUserByName);
+router.get('/login', control.isAuth, control.checkLogIn)
+router.get('/users', middleware.restrictGet, control.getUsers)
+router.get('/user/info', middleware.userDataFromToken);
 
-router.patch('/user/:id', middleware.getAuthId, control.authenticateToken, control.updateUser);
-router.delete('/user/:id', middleware.getAuthId, control.authenticateToken, control.deleteAccount);
+router.patch('/user/:username', middleware.getUserId, middleware.restrictPatch, control.updateUser);
+
+router.delete('/user/:username', middleware.getUserId, middleware.restrictDelete, control.deleteAccount);
 
 module.exports = router;
+// const express = require('express');
+// const router = express.Router();
+// const control = require('../controllers/user.controller');
+// const control2 = require('../controllers/auth.controller')
+// const middleware = require('../middlewares/middleware.auth');
+
+// // auth routes
+// router.post('/login', control.logIn);
+// router.post('/signup', control.signUp);
+
+// router.get('/user/:username', middleware.getAuthId, control.authenticateToken, control.getUserByName);
+
+// router.patch('/user/:id', middleware.getAuthId, control.authenticateToken, control.updateUser);
+// router.delete('/user/:id', middleware.getAuthId, control.authenticateToken, control.deleteAccount);
+
+// module.exports = router;
