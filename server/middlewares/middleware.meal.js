e0a04afcc3b0da821f7968c2c6f5afb7873709a0
ConnorGrace11 <70079448+ConnorGrace11@users.noreply.github.com>
2021-11-25 14:29:34 -0500

    server updates

--- server/middlewares/middleware.meal.js
+++ server/middlewares/middleware.meal.js
@@ -14,3 +14,28 @@ exports.getMealId = async (req, res, next) => {
     res.editMeal = editMeal
     next()
 }
+
+exports.getImage = async (req, res, next) => {
+    meals.findOne({}, 'img createdAt', function(error, img) {
+        if (error) return res.send(error);
+        res.contentType('json');
+        res.send(img);
+    })
+};
+
+// const meals = require('../models/meals')
+
+// exports.getMealId = async (req, res, next) => {
+//     let editMeal;
+//     try {
+//         editMeal = await meals.findById(req.params.id)
+//         if(editMeal == null) {
+//             return res.status(404).json({ message: 'cant find meal' })
+//         }
+//     } catch (error) {
+//         return res.status(500).json({ message: error.message })
+//     }
+
+//     res.editMeal = editMeal
+//     next()
+// }
