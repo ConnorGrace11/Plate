a99485338e45633dcf70ea9079c4035a7261ca96
ConnorGrace11 <70079448+ConnorGrace11@users.noreply.github.com>
2021-11-30 21:33:32 -0500

    fixed routing problem

--- src/screens/randomItems.js
+++ src/screens/randomItems.js
@@ -1,22 +1,84 @@
 import React from 'react';
 import { useEffect , useState} from "react";
-import { View, Text, Button, TouchableOpacity, FlatList, ActivityIndicator, SafeAreaView } from 'react-native';
-import axios, { Axios } from 'axios';
-import GetAllItems from './getItems';
+import { View, Text, StyleSheet, ScrollView, TextView, Dimensions, Image,ImageBackground, Button} from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import axios from 'axios';
+import rating from './rating'
 
 
+const RandomItems = ({ navigation, route }) => {
 
-const RandomItems = ({ navigation }) => {
-    return (
-      <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <GetAllItems />
+      const [restaurants, setRestaurants] = useState([]);
+      const [loading, setLoading] = useState(true);
+      const {data} = route.params;
 
-        <Button
-          title="Back to Home Screen"
-          onPress={() => navigation.navigate('Home')}
-        />
-      </SafeAreaView>
+      useEffect(()  => {
+          setTimeout(function getMeals() {
+          fetch('http://192.168.1.214:5000/restaurants/' + data + '/items')                                                    //192.168.1.214
+              .then((response) => response.json())
+              .then((json) => setRestaurants(json))
+              .catch((error) => console.error(error))
+              .finally(() => setLoading(false));
+          }, 1500)
+      }, []);
 
-    );
+      return (
+          <ScrollView>
+              {loading ? <Text style={styles.loading}>Loading Items...</Text> : restaurants.map((item) => (
+                  <View key={item._id} contentConstainerStyle={styles.container}>
+                          <Text style={styles.header}> { item.name } </Text>
+                          <Text style={styles.subHeader}> Restaurant ID: {data}</Text>
+                          <Image style={styles.image} source={{uri: item.imgMeal[0]}}/>
+                          <Text style={styles.body}> Calories: { item.calories } </Text>
+                          <Text style={styles.body}> Allergens: { item.allergens[0] } </Text>
+                          <Text style={styles.body}> Ingredients: {item.ingredients}</Text>
+                          <Text style={styles.body}> Rating Count: { item.ratingCount } </Text>
+                          <Text style={styles.body}> Subcategory: { item.subCategory } </Text>
+                          <Text style={styles.body}> Rating: { item.ratingNumber }/5 </Text>
+
+                          <Text style={styles.body}> Description: { item.description } </Text>
+                  </View>
+              ))}
+         </ScrollView>
+      )
   };
-  export default RandomItems;
+
+  const styles = StyleSheet.create({
+      container: {
+          marginTop: 50,
+          flex: 1,
+          backgroundColor: 'yellow'
+      },
+
+      header: {
+          textAlign: 'center',
+          padding: 15,
+          fontSize: 20,
+          backgroundColor: 'darkorange'
+      },
+
+      subHeader: {
+          textAlign: 'center',
+          padding: 5,
+          fontSize: 20,
+          backgroundColor: 'goldenrod'
+      },
+
+      body: {
+          textAlign: 'center',
+          padding: 5,
+          margin: 20,
+          fontSize: 15,
+      },
+
+      loading: {
+          fontSize: 35
+      },
+      image: {
+        marginTop: 10,
+        width: 200,
+        height: 200,
+      },
+  })
+export default RandomItems;
